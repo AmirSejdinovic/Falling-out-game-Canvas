d387b21093db89cf5c1a7db1aea260bd6a81de61
Amir Sejdinovic <amir24V@yahoo.com>
2021-04-15 14:28:23 +0200

    Game over function

--- game1.js
+++ game1.js
@@ -6,6 +6,11 @@ const enemies = {speed:1,arr:[],total:20};
 
 
 
+const game = {
+  play: true,
+  req: ''
+};
+
 canvas.setAttribute('height',tile*20);
 canvas.setAttribute('width',tile*25);
 canvas.style.backgroundColor='black';
@@ -41,7 +46,8 @@ document.addEventListener('keydown',(e)=>{
 document.addEventListener('keyup',(e)=>{
   if(e.code in keyz){keyz[e.code]=false;}})
 
-  requestAnimationFrame(draw);
+  gameStart(); 
+  //startScreen()
 
 function col(a,b){
     let boo = a.x<b.x+b.width && a.x + a.width > b.x && a.y<b.y+b.height && a.y + a.height > b.y;
@@ -73,6 +79,40 @@ function enemyMaker(){
   })
 }
 
+function gameOver(){
+   cancelAnimationFrame(game.req);
+   game.play = false;
+   //ctx.clearRect(0,0,canvas.width,canvas.height);
+   ctx.fillStyle = 'black';
+   ctx.fillRect(0,0, canvas.width, canvas.height);
+   let output = `GAME OVER Your Score is :  ${player.score} `;
+
+    ctx.beginPath();
+    ctx.fillStyle = '#222';
+    ctx.fillRect(10,10,canvas.width-20,50);
+    ctx.font = '24px arial';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
+    ctx.fillText(output,canvas.width/2,40);
+}
+
+function startScreen(){
+  
+  let output = `Click to Start the Game`;
+
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.fillRect(10,10,canvas.width-20,50);
+    ctx.font = '24px arial';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
+    ctx.fillText(output,canvas.width/2,canvas.height/2);
+}
+
+function gameStart(){
+  game.req = requestAnimationFrame(draw);
+  game.play = true;
+}
 
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -110,18 +150,22 @@ function draw(){
       if(enemy.growth > 10){enemy.toggle=true;enemy.growth=0;}
       enemy.size -= 1;
     }
-   
+   if(game.play){
     ctx.strokeStyle = 'white';
     ctx.arc(enemy.x+(enemy.width/2),enemy.y+(enemy.height/2),enemy.size,enemy.size,0,Math.PI*2);
     //ctx.strokeRect(enemy.x,enemy.y,enemy.width,enemy.height);
     ctx.stroke();
     ctx.fill();
     //ctx.fillRect(enemy.x,enemy.y,enemy.size,enemy.size);
-
+   }
     if(col(player,enemy)){
       let removed = enemies.arr.splice(index,1)[0];
       if(removed.bad){
          player.lives--;
+         if(player.lives < 0){
+          gameOver();
+          player.lives = '-';
+         }
       }else{
         player.score += Math.ceil(removed.size);
         console.log(removed);
@@ -129,7 +173,13 @@ function draw(){
       
 
     };
-     let output = `Live : ${player.lives} Score : ${player.score} `;
+     
+
+  })
+    
+ if(game.play){
+
+  let output = `Live(s) : ${player.lives} Score : ${player.score} `;
 
     ctx.beginPath();
     ctx.fillStyle = '#222';
@@ -138,11 +188,9 @@ function draw(){
     ctx.textAlign = 'center';
     ctx.fillStyle = 'white';
     ctx.fillText(output,canvas.width/2,40);
-
-  })
-    
- 
+  game.req = requestAnimationFrame(draw);
+ }
 
   
-  requestAnimationFrame(draw);
+ 
 }
